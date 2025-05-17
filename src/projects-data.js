cad18a5c8ca4e5b0c19fcc8b29eb3b4c3073fd05
Mauro Vargas <maurovargas7725@gmail.com>
2025-05-17 06:44:20 -0600

    Fixed iwfeatures imgage

--- src/projects-data.js
+++ src/projects-data.js
@@ -4,71 +4,70 @@ import openedRangeUrl from './assets/images/opened-range.png';
 import forbesTateUrl from './assets/images/forbes-tate.png';
 import herzogFoundationUrl from './assets/images/herzogfoundation.png';
 import smartLabelUrl from './assets/images/smart-label.png';
-import iwfeaturesUrl from './assets/images/opened-range.png';
+import iwfeaturesUrl from './assets/images/iwfeatures.png';
 import ingaaUrl from './assets/images/ingaa.png';
 import iwfUrl from './assets/images/iwf.png';
 import spnUrl from './assets/images/spn.png';
 
-const  projectsData = [
-    {
-        "name" : "Shopping Cart",
-        "image" : shoppingCartUrl,
-        "url" : "https://shopping-cart-ufxf.onrender.com",
-        "technologies" : ["Vue 3", "Vite", "Node JS", "Javascript", "Firebase Auth"]
+const projectsData = [{
+        "name": "Shopping Cart",
+        "image": shoppingCartUrl,
+        "url": "https://shopping-cart-ufxf.onrender.com",
+        "technologies": ["Vue 3", "Vite", "Node JS", "Javascript", "Firebase Auth"]
     },
     {
-        "name" : "Coalition for Cannabis Policy, Education and Regulation",
-        "image" : cpearUrl,
-        "url" : "https://www.cpear.org/",
-        "technologies" : ["WordPress", "Gravity Forms", "Yoast SEO"]
+        "name": "Coalition for Cannabis Policy, Education and Regulation",
+        "image": cpearUrl,
+        "url": "https://www.cpear.org/",
+        "technologies": ["WordPress", "Gravity Forms", "Yoast SEO"]
     },
     {
-        "name" : "Opened Range",
-        "image" : openedRangeUrl,
-        "url" : "https://openedrange.com/",
-        "technologies" : ["WordPress","Google Places API","Vue 3", "Algolia", "Google Maps", "GeoSpatial"]
+        "name": "Opened Range",
+        "image": openedRangeUrl,
+        "url": "https://openedrange.com/",
+        "technologies": ["WordPress", "Google Places API", "Vue 3", "Algolia", "Google Maps", "GeoSpatial"]
     },
     {
-        "name" : "Forbes Tate",
-        "image" : forbesTateUrl,
-        "url" : "https://forbes-tate.com/",
-        "technologies" : ["WordPress","Google reCaptcha V3", "WP Engine", "Google Analytics"]
+        "name": "Forbes Tate",
+        "image": forbesTateUrl,
+        "url": "https://forbes-tate.com/",
+        "technologies": ["WordPress", "Google reCaptcha V3", "WP Engine", "Google Analytics"]
     },
     {
-        "name" : "Herzog Foundation",
-        "image" : herzogFoundationUrl,
-        "url" : "https://herzogfoundation.com",
-        "technologies" : ["WordPress","Social media Links","Google Analytics", "Spotify Podcats",]
+        "name": "Herzog Foundation",
+        "image": herzogFoundationUrl,
+        "url": "https://herzogfoundation.com",
+        "technologies": ["WordPress", "Social media Links", "Google Analytics", "Spotify Podcats", ]
     },
     {
-        "name" : "Smart Label",
-        "image" : smartLabelUrl,
-        "url" : "https://smartlabel.org/",
-        "technologies" : ["WordPress", "Laravel","Google Analytics"]
+        "name": "Smart Label",
+        "image": smartLabelUrl,
+        "url": "https://smartlabel.org/",
+        "technologies": ["WordPress", "Laravel", "Google Analytics"]
     },
     {
-        "name" : "Indepent Women Features",
-        "image" : iwfeaturesUrl,
-        "url" : "https://www.iwfeatures.com/",
-        "technologies" : ["WordPress", "Algolia","Gravity Forms"]
+        "name": "Indepent Women Features",
+        "image": iwfeaturesUrl,
+        "url": "https://www.iwfeatures.com/",
+        "technologies": ["WordPress", "Algolia", "Gravity Forms"]
     },
     {
-        "name" : "Interstate Natural Gas Association of America",
-        "image" : ingaaUrl,
-        "url" : "https://ingaa.org/",
-        "technologies" : ["WordPress", "Algolia","Custom Themes"]
+        "name": "Interstate Natural Gas Association of America",
+        "image": ingaaUrl,
+        "url": "https://ingaa.org/",
+        "technologies": ["WordPress", "Algolia", "Custom Themes"]
     },
     {
-        "name" : "Indepent Women Forum",
-        "image" : iwfUrl,
-        "url" : "https://www.iwf.org/",
-        "technologies" : ["WordPress", "Algolia","JQuery"]
+        "name": "Indepent Women Forum",
+        "image": iwfUrl,
+        "url": "https://www.iwf.org/",
+        "technologies": ["WordPress", "Algolia", "JQuery"]
     },
     {
-        "name" : "State Policy Network",
-        "image" : spnUrl,
-        "url" : "https://spn.org/",
-        "technologies" : ["WordPress", "WP Engine","Gravity Forms"]
+        "name": "State Policy Network",
+        "image": spnUrl,
+        "url": "https://spn.org/",
+        "technologies": ["WordPress", "WP Engine", "Gravity Forms"]
     },
 ];
 
