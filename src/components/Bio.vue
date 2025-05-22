4c2bb788bdc30862452b09d55afd05514c3263dd
Mauro Vargas <maurovargas7725@gmail.com>
2025-05-22 14:40:03 -0600

    Updated Bio text

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -4,13 +4,8 @@
 			<img src="../assets/images/linkedInImage.jpeg" alt="" class="relative inline-block h-[110px] w-[110px] !rounded-full object-cover object-center mb-2">
 			<div class="flex flex-col max-w-xl">
 				<p>
-				Hi, my name is Mauro Vargas. I am a dedicated full-stack PHP developer with a strong focus on creating custom WordPress themes. My expertise includes a variety of essential plugins, such as Gravity Forms, Yoast SEO, WP Rocket, and Advanced Custom Fields, as well as developing custom plugins to meet specific client requirements. I am skilled at working with legacy code and have a proven ability to refactor it when necessary.		  </p><br>
-				<p>
-				A self-starter by nature, I excel at troubleshooting and resolving issues independently. My familiarity with a range of hosting providers, including GoDaddy, WP Engine, and Pantheon, complements my background in technical support.
-		  </p><br>
-		  <p>
-				In my daily work, I utilize a diverse set of technologies and tools, such as MySQL, Git, Webpack, Vue, Vanilla JavaScript, Twig, Figma, Lando, and Docker. I thrive in collaborative environments and have effectively partnered with project managers and designers to deliver solutions that align with clients' needs and expectations.
-		  </p>
+					I am a proud former U.S. Marine with a deep passion for coding and technology.  I hold a B.A. in Computer Science. My academic journey included comprehensive coursework in design and logic, computer systems, data structures, algorithms, databases, and web development. Over the last seven years, I have honed my skills while working with one of the largest lobbying firms in Washington, D.C. As a Software Developer, I excel at working with open software, backend servers, and APIs to meet clients' needs. I am experienced in the Google Cloud Platform(GCP) and leverage Google's API Services. I have a robust background in capturing events and transforming data into actionable insights. My ability to pivot when necessary ensures that we stay aligned with business objectives and drive impactful decisions. I take pride in my strong collaboration skills, which have allowed me to work effectively with subcontractors, project managers, and designers to deliver exceptional results.				
+				</p>
 			</div>
     </div>
 </template>
