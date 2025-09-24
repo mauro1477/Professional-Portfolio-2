ed4dde4490ff092a13fd099515132375af8199e6
Mauro Vargas <maurovargas7725@gmail.com>
2025-09-24 09:34:51 -0600

    Updated title with keywords for SEO

--- src/components/Bio.vue
+++ src/components/Bio.vue
@@ -1,19 +1,13 @@
 <template lang="">
 	<div class="sm:pt-4 sm:pb-4 pt-8 mb-8">
-		<h1 id="bio-section" class="flex justify-center font-bold text-2xl mb-2 mt-2">Bio: Mauro Vargas Jr</h1>
+		<h1 id="bio-section" class="flex justify-center font-bold text-2xl mb-2 mt-2">Mauro Vargas Jr - Professional WordPress Developer</h1>
 		<div class="flex flex-col items-center justify-center px-4">
 			<img src="../assets/images/linkedInImage_800.webp" alt="" class="relative inline-block h-[110px] w-[110px] !rounded-full object-cover object-center mb-2">
 			<div class="flex flex-col max-w-xl space-y-4 sm:space-y-6 md:space-y-8">
-				<p>
-					I am a proud former U.S. Marine and dedicated WordPress Developer with a B.A. in Computer Science. My academic foundation covered computer systems, data structures, algorithms, databases, and web development, preparing me to solve complex technical challenges with precision.				</p>
-				<p>
-					Over the past five years, I have worked as a backend and open-source software developer for one of the largest lobbying firms in Washington, D.C., as well as two digital agencies. In these roles, I specialized in Google Cloud Platform (GCP) and Google API Services, building expertise in API integrations, backend development, and event capture that transforms raw data into actionable insights.				</p>
-				<p>
-					My unique background as a U.S. Marine software engineer has instilled a disciplined, solutions-oriented mindset. I thrive in collaborative environments, working closely with subcontractors, project managers, and designers to deliver data-driven software solutions that align with evolving business objectives.				
-				</p>
-				<p>
-					With a proven track record in WordPress backend development, GCP API integrations, and digital agency projects, I bring a strong blend of technical expertise, problem-solving ability, and teamwork to every project. I am eager to continue creating innovative solutions that bridge technology and business needs.
-				</p>
+				<p>I am a proud former U.S. Marine and dedicated WordPress Developer with a B.A. in Computer Science. My academic foundation covered computer systems, data structures, algorithms, databases, and web development, preparing me to solve complex technical challenges with precision.</p>
+				<p>Over the past five years, I have worked as a backend and open-source software developer for one of the largest lobbying firms in Washington, D.C., as well as two digital agencies. In these roles, I specialized in Google Cloud Platform (GCP) and Google API Services, building expertise in API integrations, backend development, and event capture that transforms raw data into actionable insights.</p>
+				<p>My unique background as a U.S. Marine software engineer has instilled a disciplined, solutions-oriented mindset. I thrive in collaborative environments, working closely with subcontractors, project managers, and designers to deliver data-driven software solutions that align with evolving business objectives.</p>
+				<p>With a proven track record in WordPress backend development, GCP API integrations, and digital agency projects, I bring a strong blend of technical expertise, problem-solving ability, and teamwork to every project. I am eager to continue creating innovative solutions that bridge technology and business needs.</p>
 			</div>
 		</div>		
 	</div>
